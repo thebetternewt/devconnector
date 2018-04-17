fdb01fd4413f98db9b41dfc5f7a2032eb11583d2
Chris Eady <chris.eady1@gmail.com>
2018-04-17 09:44:08 -0500

    Add profile routes and validation to API

--- validation/register.js
+++ validation/register.js
@@ -12,8 +12,8 @@ module.exports = function validateRegisterInput(data) {
     ? data.password_confirmation
     : '';
 
-  if (!Validator.isLength(data.name, { min: 6, max: 30 })) {
-    errors.name = 'Name must be between 6 and 30 characters.';
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = 'Name must be between 2 and 30 characters.';
   }
   if (Validator.isEmpty(data.name)) {
     errors.name = 'Name field is required.';
